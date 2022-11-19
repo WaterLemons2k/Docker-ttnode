07d3c2c98a840d69006a5d3a1db06fef5fbb02ce
WaterLemons2k <62788816+WaterLemons2k@users.noreply.github.com>
2022-11-19 11:52:39 +0800

    Test(cron):Remove SHELL
    Remove -l option for #!/bin/bash on entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,13 @@
-#!/bin/bash -l
+#!/bin/bash
 #https://stackoverflow.com/questions/2379829/while-loop-to-test-if-a-file-exists-in-bash
 #https://github.com/ericwang2006/docker_ttnode/blob/master/build_dir/start.sh
 #https://post.smzdm.com/p/awx4rqkk/
 #https://blog.twofei.com/477/
-#https://unix.stackexchange.com/questions/308907/how-to-set-shell-bin-bash-globally-for-cron
 #变量DISABLE:0为不禁用，1为禁用签到和收星，2为禁用ttnode
 #[$(date '+%F %T')]: 当前日期和时间，示例:[1970-01-01 00:00:00]
 
 cd /data
+echo $(ps fax | grep '[c]ron' | wc -l)
 if [ $(ps fax | grep '[c]ron' | wc -l) -lt 1 ]; then #如果带cron的进程数小于1
      echo "[$(date '+%F %T')] 找不到cron，开始运行"
      cron
